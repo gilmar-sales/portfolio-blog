c7298b2b04b22d7b9513cfe4bcd005186c6ec701
gilmar.sales <gilmar.sales@zgsolucoes.com.br>
2021-05-15 14:38:37 -0300

    💄 add fira code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,10 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=KoHo:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Component key="app" {...pageProps} />
       <GlobalStyle key="global" />
