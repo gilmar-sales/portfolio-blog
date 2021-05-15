e83cf46a67c7fa24120300e7ac71cc9be5bc21c9
gilmar.sales <gilmar.sales@zgsolucoes.com.br>
2021-05-15 14:38:23 -0300

    💄 add Fira Code to code

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
 body {
   background-image: url("/grid-2x.png");
   background-repeat: repeat;
+  font-family: 'KoHo',-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
 
 a {
@@ -16,11 +17,12 @@ a {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
-  font-family: 'KoHo',-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 
 }
 
+code, pre {
+  font-family: 'Fira Code'
+}
 `;
 
 export default GlobalStyle;
