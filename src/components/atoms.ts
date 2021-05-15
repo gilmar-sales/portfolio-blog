d13bb1503b5c3a73cec496b7cabd873779691e9f
gilmar.sales <gilmar.sales@zgsolucoes.com.br>
2021-05-15 13:33:36 -0300

    💄 create atoms file

---
+++ src/components/atoms.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: #00b2ff;
+  color: black;
+  font-size: 1rem;
+  padding: 0.4rem;
+  border: 0;
+  border-radius: 0.5rem;
+
+  :hover {
+    background-color: black;
+    color: white;
+    cursor: pointer;
+  }
+`;
