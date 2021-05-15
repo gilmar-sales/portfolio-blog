4a6d8c7fe2af6bd13c05f667abd2b5c04af819a1
gilmar.sales <gilmar.sales@zgsolucoes.com.br>
2021-05-15 13:31:29 -0300

    💄 add KoHo from google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+import Head from "next/head";
 import GlobalStyle from "../styles/global";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=KoHo:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Component key="app" {...pageProps} />
       <GlobalStyle key="global" />
     </>
