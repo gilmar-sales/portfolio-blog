91668576f3f16fa5db94509c41e65c63b8cfd7b2
gilmarxd <gilmar_custodio_@hotmail.com>
2021-07-01 21:08:18 -0300

    💄 add viewport meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,10 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         ></link>
+        <meta
+          name="viewport"
+          content=" width=device-width, initial-scale=1.0, user-scalable=no"
+        />
       </Head>
       <Component key="app" {...pageProps} />
       <GlobalStyle key="global" />
