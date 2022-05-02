b2789c8bdda7a94af335c80cd30248620d9ea258
gilmarxd <gilmar_custodio_@hotmail.com>
2022-05-02 10:58:58 -0300

    🎨 prettier format

--- src/config/mdx.ts
+++ src/config/mdx.ts
@@ -1,16 +1,21 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), 'src/posts');
+export const POSTS_PATH = path.join(process.cwd(), 'src/posts')
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 // Only include md(x) files
 export const postFilePaths = fs
   .readdirSync(POSTS_PATH)
-  .filter((path) => /\.mdx?$/.test(path));
+  .filter((path) => /\.mdx?$/.test(path))
 
 export const postsSlugs = fs
   .readdirSync(POSTS_PATH)
   .filter((path) => /\.mdx?$/.test(path))
-  .map((slug) => slug.replace('.mdx', ''));
+  .map((slug) => slug.replace('.mdx', ''))
+
+export const postsTags = fs
+  .readdirSync(POSTS_PATH)
+  .filter((path) => /\.mdx?$/.test(path))
+  .map((slug) => slug.replace('.mdx', ''))
